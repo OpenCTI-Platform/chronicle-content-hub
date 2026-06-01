ed1d1006fd5cfea9e02a87d81fe7024c2fdd89a0
Romain GUIGNARD <romain.guignard@filigran.io>
2026-06-01 10:56:18 +0200

    add new actions AddObjectToContainer, CreateObservables

---
+++ content/response_integrations/third_party/community/opencti/actions/AddObjectToContainer.py
@@ -0,0 +1,88 @@
+from core.base_action import BaseAction
+from core.constants import ADD_OBJECT_TO_CONTAINER_SCRIPT_NAME
+from core.utils import parse_csv_list
+from TIPCommon.extraction import extract_action_param
+
+SUCCESS_MESSAGE = ""
+ERROR_MESSAGE = f"Error executing action {ADD_OBJECT_TO_CONTAINER_SCRIPT_NAME}"
+
+class AddObjectToContainer(BaseAction):
+    def __init__(self, script_name: str) -> None:
+        super().__init__(script_name)
+        self._result_value = True
+        self.output_message = SUCCESS_MESSAGE
+        self.error_output_message = ERROR_MESSAGE
+
+    def _extract_action_parameters(self) -> None:
+        # process action param
+
+        self.params.container_type = extract_action_param(
+            self.soar_action,
+            param_name="Container Type",
+            print_value=True,
+            is_mandatory=True,
+        )
+
+        self.params.container_id = extract_action_param(
+            self.soar_action,
+            param_name="Container Id",
+            print_value=True,
+            is_mandatory=True,
+        )
+
+        self.params.object_id = extract_action_param(
+            self.soar_action,
+            param_name="Object Id",
+            print_value=True,
+            is_mandatory=True,
+        )
+
+    def _perform_action(self, _=None) -> None:
+        # create observable with GraphQL
+
+        if self.params.container_type == "report":
+            result = self.api_client.add_object_to_report(
+                report_id=self.params.container_id,
+                object_id=self.params.object_id
+            )
+        elif self.params.container_type == "incident response case":
+            result = self.api_client.add_object_to_incident_response_case(
+                case_incident_id=self.params.container_id,
+                object_id=self.params.object_id
+            )
+        elif self.params.container_type == "grouping":
+            result = self.api_client.add_object_to_grouping(
+                grouping_id=self.params.container_id,
+                object_id=self.params.object_id
+            )
+        elif self.params.container_type == "request for information":
+            result = self.api_client.add_object_to_request_for_information(
+                rfi_id=self.params.container_id,
+                object_id=self.params.object_id
+            )
+        else:
+            raise Exception(f"Unsupported container type: {self.params.container_type}")
+
+        import json
+        self.logger.info(json.dumps(result, indent=2))
+
+        # Successful execution: the framework will keep execution_state=COMPLETED.
+        # On any raised exception, the framework will:
+        #   - log the exception
+        #   - set result_value=False and execution_state=FAILED
+        #   - build output_message from error_output_message + reason
+        self.output_message = (
+            f"Object.id {self.params.object_id} successfully added in container {self.params.container_type} with id {self.params.container_id}"
+        )
+
+def main() -> None:
+    """Entry point for executing the "Add Object To Container" action script.
+
+    This function initialized the AddObjectToContainer class with
+    the predefined script name and triggers its execution.
+    """
+    AddObjectToContainer(ADD_OBJECT_TO_CONTAINER_SCRIPT_NAME).run()
+
+if __name__ == "__main__":
+    main()
+
---
+++ content/response_integrations/third_party/community/opencti/actions/CreateObservables.py
@@ -0,0 +1,186 @@
+import json
+from core.base_action import BaseAction
+from core.constants import CREATE_OBSERVABLES_SCRIPT_NAME
+from core.utils import parse_csv_list
+from TIPCommon.extraction import extract_action_param
+from SiemplifyDataModel import EntityTypes
+from TIPCommon.utils import get_entity_original_identifier
+from SiemplifyUtils import convert_dict_to_json_result_dict
+
+SUCCESS_MESSAGE = ""
+ERROR_MESSAGE = f"Error executing action {CREATE_OBSERVABLES_SCRIPT_NAME}"
+
+SUPPORTED_ENTITIES = [
+    EntityTypes.ADDRESS,
+    EntityTypes.FILEHASH,
+    EntityTypes.URL,
+    EntityTypes.HOSTNAME,
+    EntityTypes.DOMAIN,
+    EntityTypes.USER,
+    #EntityTypes.FILENAME,
+    #EntityTypes.EMAILMESSAGE
+]
+
+entity_type_mapper = {
+    EntityTypes.HOSTNAME: "hostname",
+    EntityTypes.URL: "url",
+    EntityTypes.FILEHASH: "file",
+    EntityTypes.ADDRESS: "ip",
+    EntityTypes.USER: "email-addr",
+    EntityTypes.FILENAME: "filename",
+    EntityTypes.DOMAIN: "domain",
+    #EntityTypes.EMAILMESSAGE: "email-message"
+
+}
+
+class CreateObservables(BaseAction):
+    def __init__(self, script_name: str) -> None:
+        super().__init__(script_name)
+        self._result_value = True
+        self.output_message = SUCCESS_MESSAGE
+        self.error_output_message = ERROR_MESSAGE
+
+    def _extract_action_parameters(self) -> None:
+        # process action param
+
+        self.params.description = extract_action_param(
+            self.soar_action,
+            param_name="Description",
+            print_value=True,
+            is_mandatory=False,
+        )
+        self.params.score = extract_action_param(
+            self.soar_action,
+            param_name="Score",
+            print_value=True,
+            is_mandatory=False,
+        )
+        self.params.marking = extract_action_param(
+            self.soar_action,
+            param_name="Marking",
+            print_value=True,
+            is_mandatory=False,
+        )
+        labels = extract_action_param(
+            self.soar_action,
+            param_name="Labels",
+            print_value=True,
+            is_mandatory=False,
+        )
+        self.params.create_indicator = extract_action_param(
+            self.soar_action,
+            param_name="Create Indicator",
+            input_type=bool,
+            print_value=True,
+            is_mandatory=False,
+
+        )
+        self.params.labels = parse_csv_list(labels)
+
+    def _perform_action(self, _=None) -> None:
+        # create observables with GraphQL for all supported entities in this run
+        successful_entities = []
+        failed_entities = []
+        #json_results = []
+        self.json_results = {}
+
+        self.logger.info("target entities")
+        self.logger.info(self.soar_action.target_entities)
+
+        for entity in self.soar_action.target_entities:
+            self.logger.info(entity.entity_type)
+
+        suitable_entities = [
+            entity
+            for entity in self.soar_action.target_entities
+            if entity.entity_type in SUPPORTED_ENTITIES
+        ]
+        self.logger.info("suitable entities")
+        self.logger.info(suitable_entities)
+        for entity in suitable_entities:
+            self.logger.info("entity")
+            self.logger.info(entity)
+            identifier = get_entity_original_identifier(entity)
+            self.logger.info(identifier)
+            try:
+                observable_type = entity_type_mapper[entity.entity_type]
+                self.logger.info(observable_type)
+                result = self.api_client.create_observable(
+                    obs_value=identifier,
+                    obs_type=observable_type,
+                    obs_desc=self.params.description,
+                    obs_labels=self.params.labels,
+                    obs_score=self.params.score,
+                    marking_str=self.params.marking,
+                    create_indicator=self.params.create_indicator,
+                )
+                self.logger.info(result)
+
+                self.logger.info(
+                    f"Observable created for '{identifier}' ({observable_type})\n"
+                    f"{json.dumps(result, indent=2)}"
+                )
+
+                successful_entities.append(identifier)
+                self.json_results.update({identifier: result})
+                #json_results.append(result)
+
+            except Exception as err:
+                self.logger.exception(
+                    f"Failed to create observable for entity '{identifier}'. Error: {err}"
+                )
+                failed_entities.append(identifier)
+
+        if not suitable_entities:
+            self.output_message = "No supported entities were found to create observables."
+            self.result_value = False
+            self.json_results = {}
+            return
+
+        if successful_entities:
+            self.json_results = convert_dict_to_json_result_dict(self.json_results)
+            self.output_message += (
+                f"Successfully created observables for "
+                f"entities in {CREATE_OBSERVABLES_SCRIPT_NAME}: \n"
+                f" {', '.join(successful_entities)} \n"
+            )
+            if failed_entities:
+                self.output_message += (
+                    f"Action wasn't able to create observable for the following "
+                    f"entities in {CREATE_OBSERVABLES_SCRIPT_NAME}:"
+                    f" \n {', '.join(failed_entities)} \n"
+                )
+        else:
+            self.result_value = False
+            self.output_message = (
+                "No observables were created for the provided entities"
+                f" in {CREATE_OBSERVABLES_SCRIPT_NAME}."
+            )
+
+        """
+        output_parts = []
+        if successful_entities:
+            output_parts.append(
+                "Successfully created observables for: "
+                + ", ".join(successful_entities)
+            )
+        if failed_entities:
+            output_parts.append(
+                "Failed to create observables for: "
+                + ", ".join(failed_entities)
+            )
+
+        self.output_message = "\n".join(output_parts)
+        """
+        self.logger.info(json.dumps(self.json_results))
+
+def main() -> None:
+    """Entry point for executing the "Create Observables" action script.
+
+    This function initialized the CreateObservables class with
+    the predefined script name and triggers its execution.
+    """
+    CreateObservables(CREATE_OBSERVABLES_SCRIPT_NAME).run()
+
+if __name__ == "__main__":
+    main()
